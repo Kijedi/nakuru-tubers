04b752f1ffcccba66909e3a99325b171c0e1fdbd
Wesley KIjedi <wkijedi@gmail.com>
2024-12-18 08:55:18 +0300

    Yes

--- components/UI/Traction.jsx
+++ components/UI/Traction.jsx
@@ -24,17 +24,19 @@ export default function Traction() {
   const isInView = useInView(ref);
 
   return (
-    <div className="lg:p-12 bg-white">
-      <ul className="grid grid-cols-6 max-w-4xl mx-auto border rounded-lg shadow-lg">
+    <div ref={ref} className="lg:p-12 bg-white">
+      <ul className="grid grid-cols-6 max-w-4xl mx-auto border border-gray-700 rounded-lg shadow-lg">
         {numbers.map((number, index) => (
           <li
             key={index}
-            className=" col-span-3 lg:col-span-2   lg:m-0 flex flex-col lg:p-2 bg-white "
+            className=" col-span-3 lg:col-span-2 rounded-lg lg:m-0 flex flex-col lg:p-2 bg-white"
           >
-            <p className="text-5xl flex lg:text-7xl py-4 text-gray-400  px-4 tracking-wide uppercase">
-              {isInView && <CountUp delay={1} end={number.number} />} +
+            <p className="text-5xl flex lg:text-7xl py-4 text-gray-400 px-4 tracking-wide uppercase">
+              {isInView && 
+              <CountUp delay={1} end={number.number} />
+              } +
             </p>
-            <p className="text-md pb-6 lg:text-lg font-bold  text-emerald-600   px-4 ">
+            <p className="text-md pb-6 lg:text-lg font-bold  text-emerald-600 px-4">
               {number.name}
             </p>
           </li>
