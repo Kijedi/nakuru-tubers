7460ff82c4f61555d6b027cdfa922423e7c92f2a
Wesley KIjedi <wkijedi@gmail.com>
2025-01-04 11:24:37 +0300

    value

--- components/UI/Value.jsx
+++ components/UI/Value.jsx
@@ -12,7 +12,7 @@ const values = [
     name: "Innovation",
     icon: LuLightbulb,
     description:
-      "Continuously improving potato farming through science andtechnology.",
+      "Continuously improving potato farming through science and technology.",
   },
   {
     name: "Sustainability",
@@ -50,10 +50,10 @@ export default function Value() {
           return (
             <div
               key={index}
-              className="p-4 md:p-8 cursor-pointer border group shadow-lg my-6 md:my-0 hover:bg-emerald-600 hover:text-white ease-in-out hover:-translate-y-2 transition-all duration-200 rounded-lg border-emerald-600"
+              className="p-4 md:p-8 cursor-pointer border group shadow-lg text-gray-500 my-6 md:my-0 first:bg-emerald-600 first:text-white hover:bg-emerald-600 hover:text-white ease-in-out hover:-translate-y-2 transition-all duration-200 rounded-lg border-emerald-600"
             >
-              <Icon className="size-12 md:size-16 group-hover:text-amber-500 duration-200 ease-in-out text-emerald-600" />
-              <p className="text-xl py-2 group-hover:text-amber-500 duration-200 ease-in-out font-semibold text-emerald-600">{value.name}</p>
+              <Icon className="size-12 md:size-16 group-hover:text-amber-500 duration-200 ease-in-out " />
+              <p className="text-xl py-2 group-hover:text-amber-500 text-emerald-600 group-first:text-white duration-200 ease-in-out font-semibold">{value.name}</p>
               <p className="text-lg">{value.description}</p>
             </div>
           );
