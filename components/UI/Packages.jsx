978a8b4a00b3f8082e04ca9cac5e018dac82af81
Wesley KIjedi <wkijedi@gmail.com>
2025-01-20 17:04:39 +0300

    Contact

--- components/UI/Packages.jsx
+++ components/UI/Packages.jsx
@@ -103,7 +103,7 @@ export default function Price({ service }) {
                 {" "}
                 <Link
                   class="flex items-center group py-3  z-10 relative transition-all duration-200 px-[22px] rounded-md bg-gray-900 group-even:bg-white group-even:text-black text-white  hover:-translate-y-2 border-[2px] border-[#171B24]"
-                  href="contact"
+                  href="/contact"
                 >
                   <span class="block text-inherit w-full h-full rounded-md text-lg font-chivo font-semibold">
                     Get in Touch
