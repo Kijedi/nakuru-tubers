8bb2b721e8b5cb455c131b17a4c04efcaf2829be
Wesley KIjedi <wkijedi@gmail.com>
2025-08-18 16:58:03 +0300

    changes

--- app/market_place/page.jsx
+++ app/market_place/page.jsx
@@ -1,9 +1,9 @@
 "use client";
 import Image from "next/image";
-import Footer from "@/components/ui/Footer";
-import Navbar from "@/components/ui/Navbar";
-import { getProducts } from "@/utils/appwrite";
-import { Button } from "@/components/ui/button";
+import Footer from "../../components/ui/Footer";
+import Navbar from "../../components/ui/Navbar";
+import { getProducts } from "../../utils/appwrite";
+import { Button } from "../../components/ui/button";
 import { IoCartOutline } from "react-icons/io5";
 import {
   Popover,
