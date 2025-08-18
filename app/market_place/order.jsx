da9f156b1fa4b589350f9810e82eca22fa619533
Wesley KIjedi <wkijedi@gmail.com>
2025-08-18 21:18:23 +0300

    change

--- app/market_place/order.jsx
+++ app/market_place/order.jsx
@@ -3,9 +3,9 @@ import {
   SheetContent,
   SheetHeader,
   SheetTitle,
-} from "../../components/ui/sheet";
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input";
+} from "@/components/ui/sheet";
+import  Button  from "@/components/ui/button"
+import Input  from "@/components/ui/input";
 import { useState } from "react";
 import { LuTrash2 } from "react-icons/lu";
 import { placeOrder } from "@/utils/appwrite";
