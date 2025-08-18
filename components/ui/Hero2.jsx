2c7e1246f5d2c60715ab3e58a0ee28e282f2638d
Wesley KIjedi <wkijedi@gmail.com>
2025-08-18 22:43:56 +0300

    Chnages

--- components/ui/Hero2.jsx
+++ components/ui/Hero2.jsx
@@ -7,7 +7,7 @@ export default function Hero2() {
         <h1 className="font-bold py-3  text-emerald-600 tracking-wider w-full text-3xl lg:text-5xl capitalize">
           Nakuru Tubers
         </h1>
-        <h1 className="font-[400] pb-2  text-orange-500 tracking-wider w-full text-xl lg:text-3xl capitalize">
+        <h1 className="font-[400] pb-2  text-gray-100  tracking-wider w-full text-xl lg:text-3xl capitalize">
           Your Trusted Partner for high quality certified seed potato
         </h1>
         <p className="text-gray-300 text-xs leading-5 lg:text-sm font-light mb-5 lg:mb-8">
@@ -20,7 +20,7 @@ export default function Hero2() {
         </p>
         <Link
           alt="services"
-          className=" bg-white hidden lg:inline-block hover:bg-white/80 duration-300 border text-orange-600 ease-in-out text-sm tracking-widest  px-4 py-2"
+          className=" bg-white hidden lg:inline-block hover:bg-white/80 duration-300 border text-blackease-in-out text-sm tracking-widest  px-4 py-2"
           href={"products"}
         >
           Our Products
